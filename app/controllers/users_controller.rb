95b995e8e40bad1f3f33a70eb74679a5d8da0588
Austin Emser <austin.emser@gmail.com>
2012-07-09 16:45:09 -0400

    Added Micropost Database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
-
+  
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
+      format.xml  { render xml: @user }
     end
   end
 
